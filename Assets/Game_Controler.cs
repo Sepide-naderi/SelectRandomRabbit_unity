85888cfd73d1f9afd187f3c947f432f64d261bb3
lenovo_irg <sepidenaderi82@gmail.com>
2024-06-13 18:57:56 +0330

    work on UI more and work on sounds

--- Assets/Game_Controler.cs
+++ Assets/Game_Controler.cs
@@ -48,5 +48,135 @@ public class Game_Controler : MonoBehaviour
         InitializeGame();
     }
 
-    
+    private void InitializeGame()
+    {
+        AssignRandomColors(); 
+    }
+
+    private void AssignRandomColors()
+    {
+        int[] randomIndices = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            randomIndices[i] = UnityEngine.Random.Range(0, ColorsList.Count);
+        }
+
+        if (randomIndices[0] != randomIndices[1] && randomIndices[0] != randomIndices[2] && randomIndices[0] != randomIndices[3] &&
+            randomIndices[1] != randomIndices[2] && randomIndices[1] != randomIndices[3] &&
+            randomIndices[2] != randomIndices[3])
+        {
+            SetButtonColors(RightButtons, randomIndices[0]);
+            SetButtonColors(LeftButtons, randomIndices[1]);
+            SetButtonColors(TopButtons, randomIndices[2]);
+            SetButtonColors(BottomButtons, randomIndices[3]);
+        }
+        else
+        {
+            AssignRandomColors();
+        }
+
+        int chosenColorIndex = UnityEngine.Random.Range(0, randomIndices.Length);
+        targetIndex = randomIndices[chosenColorIndex];
+
+        DisplayColorName.text = ColorsList[targetIndex].Label;
+        Invoke("StartGameAfterDelay", delayTime);
+    }
+
+    private void SetButtonColors(Image Box, int index)
+    {
+        Box.GetComponent<Image>().color = ColorsList[index].ColorValue;
+
+        Box.GetComponent<Butten_Controler>().ColorLabel = ColorsList[index].Label;
+
+    }
+
+    private void StartGameAfterDelay()
+    {
+        isTimerRunning = true;
+    }
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void ValidateColor(string selectedColor)
+    {
+        if (selectedColor == ColorsList[targetIndex].Label)
+        {
+            SuccessSound.Play();
+
+            UpdateScore(5);
+            MaxTime = 10;
+        }
+        else
+        {
+            FailSound.Play();
+            UpdateScore(-5);
+            MaxTime = 10;
+        }
+
+        AssignRandomColors();
+    }
+
+    private void UpdateScore(int change)
+    {
+        CurrentScore += change;
+
+        if (CurrentScore >= 100)
+        {
+            Win_Panel.SetActive(true);
+            ScoreDisplay.text = CurrentScore.ToString();
+            isTimerRunning = false;
+        }
+        else
+        {
+
+
+
+            if (CurrentScore <= 0)
+            {
+                CurrentScore = 0;
+
+                Loss_Panel.SetActive(true);
+
+                ScoreDisplay.text = CurrentScore.ToString();
+                isTimerRunning = false;
+
+            }
+            else
+            {
+                ScoreDisplay.text = CurrentScore.ToString();
+                isTimerRunning = false;
+
+                TimerDisplay.text = "";
+
+            }
+
+            CancelInvoke();
+        }
+    }
+
+    private void UpdateTimer()
+    {
+        if (isTimerRunning)
+        {
+            MaxTime -= Time.deltaTime;
+
+            if (MaxTime <= 0)
+            {
+                UpdateScore(-5);
+                MaxTime = 5;
+                AssignRandomColors();
+            }
+
+            TimerDisplay.text = "Timer: " + MaxTime.ToString("N2");
+        }
+    }
+
+
+    public void Reset_The_Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
